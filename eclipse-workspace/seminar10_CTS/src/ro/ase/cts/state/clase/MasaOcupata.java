7711c3dcd56b094a237cf863ce5e65c41a9fa048
Andreea <Andreea@DESKTOP-40B08IL>
2021-05-06 07:29:59 -0700

    Command&Chain&State

---
+++ eclipse-workspace/seminar10_CTS/src/ro/ase/cts/state/clase/MasaOcupata.java
@@ -0,0 +1,16 @@
+package ro.ase.cts.state.clase;
+
+public class MasaOcupata implements Stare{
+
+	@Override
+	public void modificaStare(Masa masa) {
+		if(masa.getStare() instanceof MasaOcupata) {
+			System.out.println("Masa cu nr "+masa.getNrMasa()+" este ocupata deja");
+		}
+		else {
+			masa.setStare(this);
+			System.out.println("Masa cu nr "+masa.getNrMasa()+" s-a ocupat");
+		}
+	}
+
+}
